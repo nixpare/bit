7ba7dcb4bd91e170c9573d45e4a3f1cd3a24bf7b
Alessio Pareto (Desktop-Pare) <alessio.pareto@nixpare.com>
2024-05-22 13:18:15 +0200

    Removed fundamentally wrong functions

--- convert.go
+++ convert.go
@@ -1,45 +1,5 @@
 package bitio
 
-func UintFromBits(b []Bit) uint {
-    var sum uint
-    for i, bit := range b {
-        if bit == ONE {
-            sum += 1 << i
-        }
-    }
-    
-    return sum
-}
-
-func UintFromByters[T Byter](b []T) uint {
-    var sum uint
-    for _, v := range b {
-        sum += UintFromBits(Bits(v, LITTLE_ENDIAN))
-    }
-    
-    return sum
-}
-
-func IntFromBits(b []Bit) int {
-    var sum int
-    for i, bit := range b {
-        if bit == ONE {
-            sum += 1 << i
-        }
-    }
-    
-    return sum
-}
-
-func IntFromByters[T Byter](b []T) int {
-    var sum int
-    for _, v := range b {
-        sum += IntFromBits(Bits(v, LITTLE_ENDIAN))
-    }
-    
-    return sum
-}
-
 func BitFromByte(b byte, pos int) Bit {
 	if pos >= 8 {
 		panic("a byte has only 8 bits")
